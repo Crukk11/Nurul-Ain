e4d69bed95590e02697ced2f1d315f21935bbdae
Phyco-Ninja <60768764+Phyco-Ninja@users.noreply.github.com>
2020-06-01 23:50:47 +0530

    meh
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>

--- stdplugins/ping.py
+++ stdplugins/ping.py
@@ -1,14 +1,15 @@
 from telethon import events
 from datetime import datetime
-from uniborg.util import admin_cmd
+from uniborg.util import admin_cmd, edit_or_reply
 
 
-@borg.on(admin_cmd(pattern="ping"))
+@borg.on(admin_cmd(pattern="ping", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
+    ed = await edit_or_reply(event, event.from_id, "...")
     start = datetime.now()
-    await event.edit("Ping is High AF 😭")
+    await ed.edit("Pong!")
     end = datetime.now()
-    ms = (end - start).microseconds / 10000
-    await event.edit("Ping is High AF 😭\n{}".format(ms))
+    ms = (end - start).microseconds / 1000
+    await ed.edit("**Pong!**\n`{}` __ms__".format(ms))
